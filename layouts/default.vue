fb3fee2b6c74ab06436c387771872deb844d6604
Douglas Freitas <douglas.freitas@gou.cooking>
2018-09-14 04:42:58 -0300

    skeleton

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
-    <nuxt/>
+    <top-header/>
+    <main id="main" role="main">
+      <nuxt/>
+    </main>
+    <bottom-footer/>
   </div>
 </template>
+
+<script>
+import TopHeader from '~/components/TopHeader.vue'
+import BottomFooter from '~/components/BottomFooter.vue'
+
+export default {
+  components: {
+    TopHeader,
+    BottomFooter
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/styles.scss';
+</style>
 
 <style>
 html {
